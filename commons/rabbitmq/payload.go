087db603fbea135d3fda35390b0cd0399b3845e4
Andi Usman Balo <andi.usman@efishery.com>
2023-03-12 20:05:55 +0700

    (commons): remove payload file

--- commons/rabbitmq/payload.go
+++
@@ -1,8 +0,0 @@
-package rabbitmq
-
-type CoreNewRegisteredUserPayload struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
-	VerifyURL string `json:"verify_url"`
-}
